1c06abb768d187e299f7354c0f8a2b3f063b4ea3
IGAL SALOMON DUJOVNE <49836363@ort>
2026-07-15 08:55:06 -0300

    terminado

--- obj/Debug/net9.0/TP4.AssemblyInfo.cs
+++ obj/Debug/net9.0/TP4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c96c8eda0912cba9d36502ccb07e3010d46eddb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e147ffd48a431022228c74f9490284c83aa8a54a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
